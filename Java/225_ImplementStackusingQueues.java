7804d25ed1707b882bbc57784f8c8ac49747835f
CathySum <suncanhi@qq.com>
2017-01-13 21:57:55 +0800

    225 first commit

---
+++ Java/225_ImplementStackusingQueues.java
@@ -0,0 +1,18 @@
+/*
+@author Cathy
+@version 1.0
+RunTime:6ms
+
+题目：Rectangle Area(Difficulty: Easy)
+Description:
+
+Find the total area covered by two rectilinear rectangles in a 2D plane.
+Each rectangle is defined by its bottom left corner and top right corner as shown in the figure.
+Assume that the total area is never beyond the maximum possible value of int.
+
+解题思路：
+1、首先找到规律，本题主要考察的是数学的相关知识。
+2、本题的关键是找到两个矩形重叠的部分。运用相应位置最大值与最小值找出单个维度的重叠长度。
+
+注意事项：
+*/
