444ea3e1970cc919827ac183d75bb844d4dc84dd
CathySum <suncanhi@qq.com>
2017-07-26 19:28:18 +0800

    a new question

---
+++ Mysql/176_SecondHighestSalary.sql
@@ -0,0 +1,29 @@
+﻿/*
+@author Cathy
+@version 1.0
+
+题目：Second Highest Salary(Difficulty: Easy)
+Write a SQL query to get the second highest salary from the Employee table.
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the query should return 200 as the second highest salary. 
+If there is no second highest salary, then the query should return null.
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+
+
+解题思路
+1、排序 order by Salary desc
+2、取对应行 limit 1,1
+3、没有的话返回null 双层select
+*/
+
+select (select distinct Salary from Employee order by Salary desc limit 1,1 )  as SecondHighestSalary
