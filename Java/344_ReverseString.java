a289221050544da85a649b1b1201bee0b02dbbc5
CathySum <suncanhi@qq.com>
2017-08-22 16:32:22 +0800

    a new question

---
+++ Java/344_ReverseString.java
@@ -0,0 +1,22 @@
+/*
+@author Cathy
+@version 1.0
+RunTime:4ms
+
+题目：Reverse String(Difficulty: Easy)
+Write a function that takes a string as input and returns the string reversed.
+
+Example:
+Given s = "hello", return "olleh".
+
+解题思路：
+1、new StringBuffer(s).reverse().toString()
+*/
+
+class Solution {
+    public String reverseString(String s) {
+        String reverseS=new StringBuffer(s).reverse().toString();
+        
+        return reverseS;
+    }
+}
